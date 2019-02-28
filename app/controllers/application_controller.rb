c6ebb8a518b4e0850c72c0cdef91d8a2df0d954d
Tyler Sell <tylerselldesign@gmail.com>
2019-02-28 04:50:22 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
  require 'pry'
 require_relative '../../config/environment'
 
@@ -54,3 +55,56 @@ class ApplicationController < Sinatra::Base
     erb :edit
   end
 end
+=======
+
+require_relative '../../config/environment'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+  end
+
+  get '/' do
+    redirect to "/articles"
+  end
+  
+  get '/articles/new' do 
+    erb :new 
+  end
+  
+  post '/articles' do 
+    Article.create(params)
+    
+    redirect to "/articles/#{Article.last.id}"
+  end
+  
+  get '/articles' do 
+    @articles = Article.all 
+    erb :index
+  end
+  
+  get '/articles/:id' do 
+    @article = Article.find_by_id(params[:id])
+    erb :show 
+  end
+  
+  get '/articles/:id/edit' do 
+    @article2 = Article.find_by_id(params[:id])
+    erb :edit 
+  end
+  
+  patch '/articles/:id' do 
+    @article = Article.find(params[:id])
+    @article.update(:title => params[:title],:content => params[:content])
+    @article.save
+    redirect to "/articles/#{@article.id}"
+  end
+  
+  delete '/articles/:id/delete' do 
+    Article.find(params[:id]).destroy
+    erb :index
+  end
+end
+>>>>>>> 5153cf316a9367488ed92a9fe89a0ff5480bf61d
